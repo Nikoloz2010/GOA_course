60d37d880dc5bc9cff686dde4e916acfb99aa595
Nikoloz2010 <nikoloz2000novikov@gmail.com>
2025-02-21 07:22:14 -0800

    commit

---
+++ day 04/classwork/lesson4.py
@@ -0,0 +1,6 @@
+fullname = "Nikoloz Novikov"
+hobby = "Football" 
+print(fullname)
+print(hobby)
+print(27)
+print(20+7)
---
+++ day 04/classwork/level.py
@@ -0,0 +1,5 @@
+user = "Nikoloz"
+level = 1
+print("level up +2")
+level = 3
+
